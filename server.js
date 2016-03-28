1b906d710f1c642f16aa5a7c91522fb3d5d9100d
JamesWClark <jwclark@rockhursths.edu>
2016-03-28 09:37:49 -0500

    add a couple more messaging features

--- server.js
+++ server.js
@@ -13,21 +13,35 @@ var server = http.createServer(function (req,res) {
 
 console.log('Server on http://0.0.0.0:3000/');
 
-// socket.io
 var io = require('socket.io').listen(server);
 
-// handle socket connections
+// handles socket connections
 io.sockets.on('connection', function(socket) {
     currentConnections++;
     totalConnections++;
     
     console.log(currentConnections + ': user connected');
     
-    // send messages to the current connecting user
+    /* OUTBOUND */
+    
     socket.emit('message', { text: 'You have connected'});
     socket.emit('users', { current: currentConnections, total: totalConnections });
     
     socket.broadcast.emit('users', { current: currentConnections, total: totalConnections });
+    
+    /* INBOUND */
+    socket.on('pinj', function() {
+        console.log('received PING. sending PONG');
+        socket.emit('ponj', { text: 'PONG'});
+    });
+    
+    /* this will receive a message from one user, log it on the server, and
+     * broadcast it to the rest of the currently connected users
+     */ 
+    socket.on('message', function(data) {
+        console.log('message received: ' + data.text);
+        socket.broadcast.emit('message_response', data);
+    });
     
     socket.on('disconnect', function() {
         currentConnections--;
